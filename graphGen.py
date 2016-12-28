4b4d029d845abc752c2b54f85bea406ab535326d
france194 <francio.194@gmail.com>
2016-12-28 18:55:33 +0100

    update graphGen.py

--- graphGen.py
+++ graphGen.py
@@ -1,26 +1,57 @@
 #graphGenerator python
 #The parameter e represent the max number of possible repeted edge
 #review the genearation
+import numpy
 from random import uniform
-def graphGenerator(e):
-    file_out=open("graphGenOut.dot","w")
+
+#no labels foreign and outside no sense
+labels={
+    "1": "Urban",
+    "2": "Movement",
+    "3": "Nature",
+    "4": "Social",
+    "5": "Commerical",
+    "6": "Road",
+    "7": "XXX",
+    "8": "Zone1",
+    "9": "Zone2",
+}
+
+
+def writeLables(lab,file_out):
+    for x in lab:
+        n=uniform(1,9)
+        lab[x]=labels[str(int(n))]
+        file_out.write(str(x) + "[label=\""+lab[x]+"\"]\n")
+
+
+def graphGenerator(n):
+    file_out=open("../graphGenOut.dot","w")
     file_out.write("digraph prova{\n")
-    for y in range(e):
-        print(y)
-        for x in range(10):
-            n1=uniform(1,50)
-            for z in range(10):
-                n2=uniform(1,50)
-                if(n1>n2):
+    flag=0
+    labels_node={}
+    matrix=numpy.zeros((n,n))
+    for y in range(n):
+        flag=0
+        while(flag==0):
+            n1=uniform(1,100)
+            for z in range(n):
+                n2=uniform(1,100)
+                if(n1>n2 and matrix[y][z]==0 and y!=z):
                     time=str(int(uniform(0,24)))
-                    file_out.write(str(x) + " -> " + str(z) + "  [label=\"" + time + "\"];\n")
+                    matrix[y][z]=1
+                    labels_node[y]=None
+                    labels_node[z]=None
+                    file_out.write(str(y) + " -> " + str(z) + "  [label=\"" + time + "\"];\n")
+                    flag=1
+    writeLables(labels_node,file_out)
     file_out.write("}")
     file_out.close()
 
 def main():
-    e=int(input("Dammi sta thr -- >"))
-    graphGenerator(e)
-    print("AA")
+    n=int(input("Dammi er numero de nodi --> "))
+    graphGenerator(n)
+    print("Graph Generated")
 
 if __name__ == "__main__":
     main()
